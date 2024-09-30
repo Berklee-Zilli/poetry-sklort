0e7cd478428501a098ffc71e5dee308221144d11
Toby Hurrell <tobyhurrell@Tobys-MacBook-Pro-2.local>
2024-09-30 18:46:46 -0400

    Revised HW Python

---
+++ python-hw-1.py
@@ -0,0 +1,17 @@
+# Toby Hurrell
+# LMSC261 Intro to Programming
+# September 30, 2024
+
+'''The Vengabus is coming
+And everybody's jumping
+New York to San Francisco
+An intercity disco
+The wheels of steel are turning
+And traffic lights are burning
+So if you like to party
+Get on and move your body'''
+
+semester = 7
+integer_one = 1
+float_one = 1.0 
+string_one = "1"
